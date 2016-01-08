33dbf694d8d65df28b20e5a0bdf792397f3d5819
Konstantin Tokarev <annulen@yandex.ru>
2016-01-08 22:30:22 +0300

    Coding style fixes
    Change-Id: I0f1c40d9fcb68bda2425b7677866e3abccc9f9ef

--- Source/WTF/wtf/text/AtomicString.h
+++ Source/WTF/wtf/text/AtomicString.h
@@ -174,7 +174,9 @@ public:
     operator NSString*() const { return m_string; }
 #endif
 #if PLATFORM(QT)
-    AtomicString(const QString& s) : m_string(add(String(s).impl())) { }
+    AtomicString(const QString& s)
+        : m_string(add(String(s).impl()))
+    { }
     operator QString() const { return m_string; }
 #endif
 
