e35664b59726e6f127c79ce13e600488ffadf39c
Konstantin Tokarev <annulen@yandex.ru>
2016-02-13 20:56:56 +0300

    Fixed compilation of QtWebKit with USE_GLIB.
    Change-Id: Ia6638e0568a1bce382662f030759f1f743ef17ae

--- Source/WTF/wtf/RunLoop.h
+++ Source/WTF/wtf/RunLoop.h
@@ -68,7 +68,7 @@ public:
     WTF_EXPORT_PRIVATE void runForDuration(double duration);
 #endif
 
-#if USE(GLIB) && !PLATFORM(EFL)
+#if USE(GLIB) && !PLATFORM(EFL) && !PLATFORM(QT)
     WTF_EXPORT_PRIVATE GMainContext* mainContext() const { return m_mainContext.get(); }
 #endif
 
@@ -87,7 +87,7 @@ public:
 
         virtual void fired() = 0;
 
-#if USE(GLIB) && !PLATFORM(EFL)
+#if USE(GLIB) && !PLATFORM(EFL) && !PLATFORM(QT)
         void setPriority(int);
 #endif
 
