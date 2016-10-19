a9db71827cc89db67010fa006b378f28fb40bac6
Cerri <maceraico@gmail.com>
2016-10-19 12:53:01 +0200

    Fixed headers order

--- Source/WebCore/platform/network/ParsedContentRange.cpp
+++ Source/WebCore/platform/network/ParsedContentRange.cpp
@@ -22,12 +22,12 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
-#define __STDC_FORMAT_MACROS
-#include <inttypes.h>
 
+#define __STDC_FORMAT_MACROS
 #include "config.h"
 #include "ParsedContentRange.h"
 
+#include <inttypes.h>
 #include <wtf/text/WTFString.h>
 
 namespace WebCore {
