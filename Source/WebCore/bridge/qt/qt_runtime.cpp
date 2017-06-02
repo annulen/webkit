01cd3a45f6712bd8b157c9c49768a977b10d42ba
Herb Miller Jr <herb@hlmjr.com>
2017-06-01 23:48:18 -0400

    Oops. Make CI happy.

--- Source/WebCore/bridge/qt/qt_runtime.cpp
+++ Source/WebCore/bridge/qt/qt_runtime.cpp
@@ -976,7 +976,7 @@ static int indexOfMetaEnum(const QMetaObject *meta, const QByteArray &str)
 static int findMethodIndex(JSContextRef context,
                            const QMetaObject* meta,
                            const QByteArray& signature,
-                           unsigned int argumentCount,
+                           unsigned argumentCount,
                            const JSValueRef arguments[],
                            bool allowPrivate,
                            QVarLengthArray<QVariant, 10> &vars,
