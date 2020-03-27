e2ff0cfd625263b3b1f25a693fd15afd52ae4de8
RAJAGOPALAN-GANGADHARAN <g.raju2000@gmail.com>
2020-03-27 16:53:25 +0530

    Build Automation QtWebkit with Conan Revision 2

--- Tools/qt/conanfile.py
+++ Tools/qt/conanfile.py
@@ -1,13 +1,14 @@
 from conans import ConanFile, CMake, tools
 import os
 
+
 class QtWebKitConan(ConanFile):
     name = "qtwebkit"
     version = "5.212.0-alpha4"
     license = "LGPL-2.0-or-later, LGPL-2.1-or-later, BSD-2-Clause"
     url = "https://github.com/qtwebkit/qtwebkit"
     description = "Qt port of WebKit"
-    topics = ( "qt", "browser-engine", "webkit", "qt5", "qml", "qtwebkit" )
+    topics = ("qt", "browser-engine", "webkit", "qt5", "qml", "qtwebkit")
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "virtualenv"
     exports_sources = "../../*"
@@ -21,7 +22,7 @@ class QtWebKitConan(ConanFile):
 
         "libpng/1.6.37",
         "sqlite3/3.31.1"
-    )
+        )
     default_options = {
         "icu:shared": True,
         "icu:data_packaging": "library",
@@ -34,7 +35,7 @@ class QtWebKitConan(ConanFile):
         "libxslt:shared": True,
 
         "libjpeg-turbo:shared": False
-    }
+        }
 # For building with conan
 #    options = {
 #        "use_ccache": [True, False],
@@ -62,55 +63,43 @@ class QtWebKitConan(ConanFile):
         if not tools.which("flex"):
             self.build_requires("flex_installer/2.6.4@bincrafters/stable")
 
-        
-
     def build(self):
-        cmake = CMake(self,set_cmake_flags=True)
+        cmake = CMake(self, set_cmake_flags=True)
         cmake.generator = "Ninja"
         cmake.verbose = False
         cmake.definitions["QT_CONAN_DIR"] = self.build_folder
-        cmake.definitions["CMAKE_BUILD_TYPE"]="Release"
+        cmake.definitions["CMAKE_BUILD_TYPE"] = "Release"
 
-        os.environ["CC"]="cl"
-        os.environ["CXX"]="cl"
         # if self.options.use_ccache:
         #    cmake.definitions["CMAKE_C_COMPILER_LAUNCHER"] = "ccache"
         #    cmake.definitions["CMAKE_CXX_COMPILER_LAUNCHER"] = "ccache"
-        #conan build ..\Tools\qt\conanfile.py --source-folder C:\qtwebkit
 
-        if "Qt5_dir" in os.environ:
-            cmake.definitions["Qt5_DIR"] = os.environ["Qt5_dir"]
-        else:
-            self.output.error("Specify path to Qt5 directory by setting environment variable Qt5_dir")
-        
-        print(cmake.definitions["CONAN_CXX_FLAGS"])
-        
+        if "QTDIR" in os.environ:
+            cmake.definitions["Qt5_DIR"] = os.environ["QTDIR"] + r'\lib\cmake\Qt5'
+            print("Qt5 directory:" + cmake.definitions["Qt5_DIR"])
+
         #Retrieve cmake args set by wrapper
-        if "Temp_D_Flags_For_Dev" in os.environ:
-            ddef=os.environ["Temp_D_Flags_For_Dev"]
-            ddef_list=ddef.split(';')
+        if "DCMAKEFLAGS" in os.environ:
+            ddef = os.environ["DCMAKEFLAGS"]
+            ddef_list = ddef.split(';')
             for ddef_it in ddef_list:
-                dtemp=ddef_it.split('#')
-                if len(dtemp)==2:
-                    cmake.definitions[dtemp[0]]=dtemp[1]
+                dtemp = ddef_it.split('#')
+                if len(dtemp) == 2:
+                    cmake.definitions[dtemp[0]] = dtemp[1]
 
-        
-        if "Temp_File_FLags_For_Dev" in os.environ:
-            defs=" "+os.environ["Temp_File_FLags_For_Dev"]+" "
-            #print(cmake.definitions["CONAN_CXX_FLAGS"]+defs)
-            cmake.definitions["CONAN_CXX_FLAGS"]=defs+cmake.definitions["CONAN_CXX_FLAGS"]
+        if "CMAKEFLAGS" in os.environ:
+            defs = " " + os.environ["CMAKEFLAGS"] + " "
+            cmake.definitions["CONAN_CXX_FLAGS"] = defs + cmake.definitions["CONAN_CXX_FLAGS"]
 
         if self.settings.os == "Windows":
             print(tools.vcvars_command(self.settings))
-        
+
         print(self.source_folder)
         print()
         print(self.build_folder)
 
-        #self.run(r'cmake -DQT_CONAN_DIR=. -DCMAKE_BUILD_TYPE=Release -GNinja -DQt5_DIR=C:\Qt\5.13.1\msvc2017_64\lib\cmake\Qt5 ..')
         cmake.configure()
         cmake.build()
-        
 
     def package(self):
         pass
--- Tools/qt/qtwebkit-windows.py
+++ Tools/qt/qtwebkit-windows.py
@@ -4,52 +4,50 @@ import argparse
 from conans import tools
 import pathlib
 
+
 def parse_qt(qt):
-    if qt==None:
-        ##Scan in qmake look for Qt5Config.cmake,Qt5ConfigVersion.cmake, Qt5ModuleLocation.cmake
-        pass
-    else:
-        os.environ['Qt5_dir']=qt
+    if qt != None:
+        os.environ['QTDIR'] = qt
+
 
 def parse_cmake(cmake):
     ## Eg -Wno-dev -DFLag1=val -DFlag2=val2 -fsanitize=address
-    Dflags=""
-    flags=""
-    if cmake=="":
+    Dflags = ""
+    flags = ""
+    if cmake == "":
         return
-    cl=cmake.split(' ')
+    cl = cmake.split(' ')
     for cc in cl:
-        if cc[1]=='D':
-            tc=cc.split('=')
-            Dflags+=tc[0][2:]+"#"+tc[1]+";"
+        if cc[1] == 'D':
+            tc = cc.split('=')
+            Dflags += tc[0][2:] + "#" + tc[1] + ";"
         else:
-            flags+=cc+" "
+            flags += cc + " "
 
-    os.environ["Temp_D_Flags_For_Dev"]=Dflags
-    os.environ["Temp_File_FLags_For_Dev"]=flags
+    os.environ["DCMAKEFLAGS"] = Dflags
+    os.environ["CMAKEFLAGS"] = flags
 
-parser=argparse.ArgumentParser("build-qtwebkit")
+parser = argparse.ArgumentParser("QtWebkit building with Conan")
 
-parser.add_argument("--qt",help="Full path pointing to Qt5*.cmake it looks like <**>\Qt\5.13.1\<**>\lib\cmake\Qt5",type=str)
-parser.add_argument("--cmakeargs",help="Space seperated values that should be passed as CMake arguments",default="",type=str)
-parser.add_argument("--ninjaargs",help="Ninja arguments",default="", type=str)
-parser.add_argument("--install",help="Pass this flag if you want to invoke install script",action="store_true")
-parser.add_argument("--build_folder",help="name of build folder defaults to build",default="build",type=str)
+parser.add_argument("--qt", help="Root Directory of Qt Installation", type=str)
+parser.add_argument("--cmakeargs", help="Space seperated values that should be passed as CMake arguments", default="", type=str)
+parser.add_argument("--ninjaargs", help="Ninja arguments", default="", type=str)
+parser.add_argument("--install", help="Pass this flag if you want to invoke install script", action="store_true")
+parser.add_argument("--build_folder", help="name of build folder defaults to build", default="build", type=str)
 
+args = parser.parse_args()
 
+src_directory = str(pathlib.Path(__file__).resolve().parents[2])
+install_folder = src_directory + "\\" + args.build_folder
 
-args=parser.parse_args()
+print("Path of build directory:" + install_folder)
 
-src_directory=str(pathlib.Path(__file__).resolve().parents[2])
-install_folder=src_directory+"\\"+args.build_folder
-
-print(install_folder)
 if args.install == True:
-    script='conan install conanfile.py -if "{0}"'.format(install_folder)
+    script = 'conan install conanfile.py -if "{0}"'.format(install_folder)
     os.system(script)
 
 parse_qt(args.qt)
 parse_cmake(args.cmakeargs)
 
-script='conan build conanfile.py -sf "{0}" -bf "{1}"'.format(src_directory,install_folder)
+script = 'conan build conanfile.py -sf "{0}" -bf "{1}"'.format(src_directory, install_folder)
 os.system(script)
